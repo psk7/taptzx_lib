56ba280c40bfe7904349e213ad6703619639f0b4
Kireev Sergey <kireevsv@gmail.com>
2020-08-27 21:53:35 +0300

    Some optimizations

--- buf.go
+++ buf.go
@@ -4,44 +4,46 @@ import (
 	"io"
 )
 
-type abuf struct {
+type aBuf struct {
 	writer *io.Writer
-	ch     *chan []byte
-	ex     *chan bool
+	ch     chan *[]byte
+	ex     chan bool
 	done   bool
 }
 
-func createABuf(writer *io.Writer) *abuf {
-	c := make(chan []byte, 16)
-	ce := make(chan bool)
-	b := abuf{writer: writer, ch: &c, ex: &ce}
+func createABuf(writer *io.Writer) *aBuf {
+	b := aBuf{writer: writer, ch: make(chan *[]byte, 16), ex: make(chan bool)}
 
 	go func() {
 		for !b.done {
-			if len(*b.ch) < 16 {
+			if len(b.ch) < cap(b.ch) {
 				continue
 			}
 
-			(*b.writer).Write(<-*b.ch)
+			(*b.writer).Write(*<-b.ch)
 		}
 
-		for len(*b.ch) > 16 {
-			(*b.writer).Write(<-*b.ch)
+		for len(b.ch) > 0 {
+			(*b.writer).Write(*<-b.ch)
 		}
 
-		*b.ex <- true
+		b.ex <- true
 	}()
 
 	return &b
 }
 
-func (b *abuf) WaitComplete() {
-	b.done = true
-
-	<-*b.ex
+func (buf *aBuf) WaitComplete() {
+	buf.done = true
+	<-buf.ex
 }
 
-func (a *abuf) Write(p []byte) (n int, err error) {
-	*a.ch <- p
+func (buf *aBuf) Write(p []byte) (n int, err error) {
+	if !buf.done {
+		cp := make([]byte, len(p))
+		copy(cp, p)
+		buf.ch <- &cp
+	}
+
 	return len(p), nil
 }
