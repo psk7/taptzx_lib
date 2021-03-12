07fc4c176117ac52099886de3962f50eba2a51f1
Kireev Sergey <kireevsv@gmail.com>
2021-03-12 17:37:01 +0300

    Some equalisation added

--- lib.go
+++ lib.go
@@ -5,6 +5,7 @@ import (
 	"container/list"
 	"errors"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"strings"
@@ -25,14 +26,17 @@ type audioStream interface {
 }
 
 type astream struct {
-	cpuFreq      uint64
-	freq         uint64
-	cpuTimeStamp uint64
-	sndTimeStamp uint64
-	currentLevel bool
-	cpuTimeBase  uint64
-	sndTimeBase  uint64
-	wr           *bufio.Writer
+	cpuFreq         uint64
+	freq            uint64
+	cpuTimeStamp    uint64
+	sndTimeStamp    uint64
+	currentLevel    bool
+	currentVol      int16
+	lastRiseSamples uint64
+	maxRiseSamples  uint64
+	cpuTimeBase     uint64
+	sndTimeBase     uint64
+	wr              *bufio.Writer
 }
 
 type context struct {
@@ -49,11 +53,46 @@ func (b *baseBlock) generate(stream audioStream) {
 func (s *astream) appendLevel(len int, lvl int16) {
 	s.cpuTimeStamp += uint64(len) * s.cpuTimeBase
 
+	// Emit rise or fall
+	if s.currentVol != lvl {
+		riseSamples := ((s.cpuTimeStamp - s.sndTimeStamp) / s.sndTimeBase) / 2
+
+		if riseSamples > s.maxRiseSamples {
+			riseSamples = s.maxRiseSamples
+		}
+
+		actualRiseSamples := riseSamples
+		if actualRiseSamples > s.lastRiseSamples {
+			actualRiseSamples = s.lastRiseSamples
+		}
+
+		s.lastRiseSamples = riseSamples
+
+		if actualRiseSamples > 0 {
+			phase := 0.0
+			phaseStep := math.Pi / float64(actualRiseSamples)
+			amp := float64(int32(lvl) - int32(s.currentVol))
+
+			for i := 0; i < int(actualRiseSamples); i++ {
+				v := int16((-math.Cos(phase)+1)/2*amp + float64(s.currentVol))
+				s.wr.WriteByte(byte(v % 256))
+				s.wr.WriteByte(byte(v >> 8))
+
+				phase += phaseStep
+
+				s.sndTimeStamp += s.sndTimeBase
+			}
+		}
+	}
+
+	// Emit sustain
 	for s.sndTimeStamp < s.cpuTimeStamp {
 		s.wr.WriteByte(0)
 		s.wr.WriteByte(byte(lvl >> 8))
 		s.sndTimeStamp += s.sndTimeBase
 	}
+
+	s.currentVol = lvl
 }
 
 func (s *astream) addEdge(len int) {
@@ -107,11 +146,18 @@ func OpenFile(fileName string) (*context, error) {
 	return &c, nil
 }
 
-func (c *context) GenerateAudioTo(writer io.Writer, freq int, trace func(string)) {
-	bb := createABuf(&writer)
-
+func (c *context) GenerateAudioTo(writer io.Writer, freq int, sync bool, trace func(string)) {
 	stream := astream{freq: uint64(freq), cpuFreq: 3500000, currentLevel: false}
-	stream.wr = bufio.NewWriter(bb)
+	stream.maxRiseSamples = uint64(float32(0.00015) * float32(freq)) // 150 mkSec
+
+	var bb *aBuf
+
+	if sync {
+		stream.wr = bufio.NewWriter(writer)
+	} else {
+		bb = createABuf(&writer)
+		stream.wr = bufio.NewWriter(bb)
+	}
 
 	timeBase := getLCM(uint32(stream.freq), uint32(stream.cpuFreq))
 	stream.cpuTimeBase = timeBase / stream.cpuFreq
@@ -131,5 +177,7 @@ func (c *context) GenerateAudioTo(writer io.Writer, freq int, trace func(string)
 
 	stream.wr.Flush()
 
-	bb.WaitComplete()
+	if bb != nil {
+		bb.WaitComplete()
+	}
 }
